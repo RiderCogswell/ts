92b79d35bf198e38a75a5dda470f0a137381acf4
Ridercogswell <ridercogswell@gmail.com>
2022-07-26 14:13:04 -0700

    refactor comment

--- generics.ts
+++ generics.ts
@@ -27,6 +27,6 @@ class ArrayOfAny<T> {
 new ArrayOfAny<number> ([1, 4, 5, 77, 9])
 new ArrayOfAny<string> (['hello', 'world'])
 
-// TS generics allow for type inference when declared through the values given
+// TS generics allow for type inference when declared through the given values
 const arr = new ArrayOfAny(['a', 'b', 'c'])
 const arrN = new ArrayOfAny([1, 2, 3])
