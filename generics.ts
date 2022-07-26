77423d78083fa8aeb1852a53479a5b19c83d5f88
Ridercogswell <ridercogswell@gmail.com>
2022-07-26 14:12:24 -0700

    generic type inference

--- generics.ts
+++ generics.ts
@@ -26,3 +26,7 @@ class ArrayOfAny<T> {
 // where we can pass in whatever type we like through this function
 new ArrayOfAny<number> ([1, 4, 5, 77, 9])
 new ArrayOfAny<string> (['hello', 'world'])
+
+// TS generics allow for type inference when declared through the values given
+const arr = new ArrayOfAny(['a', 'b', 'c'])
+const arrN = new ArrayOfAny([1, 2, 3])
