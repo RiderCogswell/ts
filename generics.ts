8220dde4e7bae416f17563c3f1a01fd983d4882e
Ridercogswell <ridercogswell@gmail.com>
2022-07-26 14:08:48 -0700

    generics

---
+++ generics.ts
@@ -0,0 +1,28 @@
+class ArrayOfNumber {
+  constructor(public collection: number[]) {}
+
+  get(index: number): number {
+    return this.collection[index];
+  }
+}
+
+class ArrayOfStrings {
+  constructor(public collection: string[]) {}
+
+  get(index: number): string {
+    return this.collection[index]
+  }
+}
+
+// think of generics literally as arguments for types,
+class ArrayOfAny<T> {
+  constructor(public collection: T[]) {}
+
+  get(index: number): T {
+    return this.collection[index]
+  }
+}
+
+// where we can pass in whatever type we like through this function
+new ArrayOfAny<number> ([1, 4, 5, 77, 9])
+new ArrayOfAny<string> (['hello', 'world'])
