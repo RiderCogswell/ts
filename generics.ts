13729a98ae0e23215e0cdf2927f3596f864940b7
Ridercogswell <ridercogswell@gmail.com>
2022-07-26 18:21:47 -0700

    fin

--- generics.ts
+++ generics.ts
@@ -44,11 +44,42 @@ function printNums(arr: number[]): void {
   }
 }
 
+// works the same as with classes
 function printAny<T>(arr: T[]): void {
   for (let i = 0; i < arr.length; i++) {
     console.log(arr[i]);
   }
 }
 
+// here we would not have to write <string[]> as that would make TS expect string[][]
 printAny<string>(['a', 'b', 'c']);
 printAny([1, 2, 3, 5])
+
+// generic constraints
+
+class Car {
+  print() {
+    console.log('I am a car');
+  }
+}
+
+class House {
+  print() {
+    console.log('I am a house!');
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+// use extends to create a generic constraint, which tells T that it will have a print() method
+function printHouseOrCars<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+// pass in whichever type we want, just like arguments
+printHouseOrCars<House>([new House(), new House()]);
+printHouseOrCars<Car>([new Car(), new Car()]);
